c59457920604ac18e394b025d506a9c50bda2d8c
Horseman.mini <alexey.volosnikov@mail.ru>
2021-02-07 22:47:07 +0600

    Updates

--- src/components/Logotype/Logotype.js
+++
@@ -1,9 +0,0 @@
-import React, {Component} from 'react';
-import {Link} from "react-router-dom";
-import './logotype.scss';
-
-export class Logotype extends Component {
-    render() {
-        return <Link className={"logo "} to={this.props.to}>LIVEDUNE</Link>;
-    }
-}
